452343730f893923bd89ed8bb663a12563d79315
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2020-04-02 08:13:54 +0530

    delta 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule,NO_ERRORS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,13 +8,7 @@ import { BodyComponent } from './body/body.component';
 import { FooterComponent } from './footer/footer.component';
 import { TemplateFormComponent } from './template-form/template-form.component';
 import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { CarouselModule,
-  WavesModule,
-  NavbarModule,
-  ButtonsModule,
-  IconsModule } from 'angular-bootstrap-md';
 
 
 
@@ -32,15 +26,8 @@ import { CarouselModule,
     ReactiveFormsModule,
     AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule,
-    CarouselModule.forRoot(),
-    WavesModule.forRoot(),
-    ButtonsModule,
-    IconsModule,
-    NavbarModule,
     HttpClientModule
   ],
-  schemas: [NO_ERRORS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
