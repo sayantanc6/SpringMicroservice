eb26bf5dcd90bfaa79e9541e5ac5673652bbb9be
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2020-04-29 19:16:43 +0530

    abc

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm, FormGroup, 
           FormGroupName, FormControlName, FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { EmailValidator } from '../shared/email-validator';
 
 @Component({
   selector: 'app-reactive-form',
@@ -20,14 +21,17 @@ export class ReactiveFormComponent implements OnInit {
     this.f = this.fb.group({
       group1: this.fb.group({
         firstName: ['',[Validators.required,Validators.minLength(3),Validators.maxLength(5)]],
-        lastName: ['',[Validators.required,Validators.minLength(3),Validators.maxLength(5)]]
+        lastName: ['',[Validators.required,Validators.minLength(3),Validators.maxLength(5)]],
+        email: ['',[EmailValidator.emailDomainValidator(),Validators.required,Validators.email]]
       })
     });
   }
 
   onSubmit(f: FormGroup): void {
-    console.log(f.value);
     console.log(f);
+    console.log("first name:"+f.controls.group1.value.firstName);
+    console.log("last name:"+f.controls.group1.value.lastName);
+    console.log("email:"+f.controls.group1.value.email);
   }
 
 }
