4b6f3b849bf3a4597880e7621f720bf1cb73d071
Sayantan Chatterjee <Sayantanc6@gmail.com>
2020-07-21 20:02:25 +0530

    abc

--- ZipkinServer/src/main/java/dummy/ZipkinServerApplication.java
+++ ZipkinServer/src/main/java/dummy/ZipkinServerApplication.java
@@ -3,6 +3,9 @@ package dummy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import zipkin2.server.internal.EnableZipkinServer;
+
+@EnableZipkinServer
 @SpringBootApplication
 public class ZipkinServerApplication {
 
