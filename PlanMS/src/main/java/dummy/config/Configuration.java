a5d54bf0c7bc947bccb133f02dadbd59dab90eb5
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2020-08-07 10:52:30 +0530

    Update Configuration.java
    na

--- PlanMS/src/main/java/dummy/config/Configuration.java
+++ PlanMS/src/main/java/dummy/config/Configuration.java
@@ -5,10 +5,25 @@ import org.springframework.context.annotation.Bean;
 
 @org.springframework.context.annotation.Configuration
 public class Configuration {
+       @Bean
+    public Queue queue(){
+        return new ActiveMQQueue("test-queue");
+    }
 	
 	@Bean
 	public DozerBeanMapper mapper() {
 	    return new DozerBeanMapper();
 	}
 
+        @Bean
+	public JmsListenerContainerFactory<?> myFactory(
+	    ConnectionFactory connectionFactory,
+	    DefaultJmsListenerContainerFactoryConfigurer configurer) {
+	  DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+
+	  factory.setErrorHandler(t -> System.err.println("An error has occurred in the transaction"));
+
+	  configurer.configure(factory, connectionFactory);
+	  return factory;
+	}
 }
