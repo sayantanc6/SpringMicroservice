04898daa55b01c7155c0aeefdc8423c61783c34f
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2020-08-07 10:57:50 +0530

    Update PlanMsApplication.java
    na

--- PlanMS/src/main/java/dummy/PlanMsApplication.java
+++ PlanMS/src/main/java/dummy/PlanMsApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.jms.annotation.EnableJms;
+
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCaching
+@EnableJms
 public class PlanMsApplication {
 
 	public static void main(String[] args) {
