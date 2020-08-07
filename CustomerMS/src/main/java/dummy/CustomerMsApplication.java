3b8efe63339560ba5397cf19b8327666751b8623
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2020-08-07 10:56:51 +0530

    Update CustomerMsApplication.java
    na

--- CustomerMS/src/main/java/dummy/CustomerMsApplication.java
+++ CustomerMS/src/main/java/dummy/CustomerMsApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCaching
 @EnableFeignClients
+@EnableJms
 public class CustomerMsApplication {
 
 	public static void main(String[] args) {
